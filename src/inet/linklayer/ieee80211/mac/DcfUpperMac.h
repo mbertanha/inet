d5067a06cd6d052812a07108fb5ae7d71599e943
Rudolf Hornig <rudi@omnetpp.org>
2016-01-21 16:37:26 +0100

    build: fixed warnings

--- src/inet/linklayer/ieee80211/mac/DcfUpperMac.h
+++ src/inet/linklayer/ieee80211/mac/DcfUpperMac.h
@@ -103,7 +103,7 @@ class INET_API DcfUpperMac : public cSimpleModule, public IUpperMac, public ICon
 
     public:
         DcfUpperMac();
-        ~DcfUpperMac();
+        virtual ~DcfUpperMac();
         virtual void upperFrameReceived(Ieee80211DataOrMgmtFrame *frame) override;
         virtual void lowerFrameReceived(Ieee80211Frame *frame) override;
         virtual void corruptedFrameReceived() override;
